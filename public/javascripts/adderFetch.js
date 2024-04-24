6c00be13323e41f79440457761b5fc256a7d3ac0
noelhoppe <noel.hoppe@gmx.de>
2024-04-24 20:09:10 +0200

    ohne Aufgabe 2.3 & 2.4

--- public/javascripts/adderFetch.js
+++ public/javascripts/adderFetch.js
@@ -8,22 +8,24 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+// adder:
 const addForm = document.querySelector("#add-form");
 addForm.addEventListener("submit", (event) => __awaiter(void 0, void 0, void 0, function* () {
     event.preventDefault(); // prevent side reload when submitting the form
+    // select values
     const a = Number(document.querySelector("#a").value);
     const b = Number(document.querySelector("#b").value);
     const c = Number(document.querySelector("#c").value);
     const res = yield fetch("http://localhost:3000/add", {
         method: "POST",
-        mode: "cors", // This set CORS mode, not required herea
+        mode: "cors", // This set CORS mode, not required here but recommended
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ a, b, c }),
+        body: JSON.stringify({ a, b, c }), // übertrage a, b, und c als json zum Server
     });
-    const data = yield res.json();
-    console.log(res.status + " " + res.statusText);
+    const data = yield res.json(); // parse das zurückgegeben JSON
+    console.log(res.status + " " + res.statusText); // Gebe den http statuscode in der Konsole aus
     if (res.ok) {
         const result = document.querySelector("#result");
         if (result) {
@@ -31,6 +33,7 @@ addForm.addEventListener("submit", (event) => __awaiter(void 0, void 0, void 0, 
         }
     }
 }));
+// Aufgabe 1:
 document.addEventListener("DOMContentLoaded", () => __awaiter(void 0, void 0, void 0, function* () {
     const res = yield fetch("http://localhost:3000/cookies", {
         method: "GET", // GET request
@@ -38,4 +41,34 @@ document.addEventListener("DOMContentLoaded", () => __awaiter(void 0, void 0, vo
         credentials: "include" // setze credential auf include, um cookies zu setzen
     });
     console.log(res.status + " " + res.statusText); // Gebe den HTTP status code und den status text des fetch()-Aufrufs in der Konsole aus
+}));
+const getCookiesBtn = document.querySelector("#displayCookiesBtn");
+const displayCookies = document.querySelector("#displayCookies ");
+getCookiesBtn.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () {
+    displayCookies.textContent = document.cookie; // schreibe cookies in das feld
+}));
+const hideCookiesBtn = document.querySelector("#hideCookiesBtn");
+hideCookiesBtn.addEventListener("click", () => {
+    displayCookies.textContent = ""; // setze das Feld zurück
+});
+// Aufgabe 2:
+// Selektiere login und logout btn
+const loginBtn = document.querySelector("#login");
+const logoutBtn = document.querySelector("#logout");
+loginBtn.addEventListener("click", (evt) => __awaiter(void 0, void 0, void 0, function* () {
+    evt.preventDefault(); // prevents side reloading
+    const username = document.querySelector("#userName").value; // lese wert des Inputs Username
+    // console.log(username);
+    if (username != null) { // rufe die Fnktn. nur auf, wenn ein Wert in username steht
+        const res = yield fetch("http://localhost:3000/login", {
+            method: "POST",
+            mode: "cors", // This set CORS mode, not required here but recommended
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username }), // übertrage a, b, und c als json zum Server
+        });
+        console.log(res.status + " " + res.statusText);
+    }
 }));
--- public/javascripts/adderFetch.ts
+++ public/javascripts/adderFetch.ts
@@ -1,22 +1,24 @@
+// adder:
 const addForm : HTMLFormElement = document.querySelector("#add-form") as HTMLFormElement;
 addForm.addEventListener("submit", async (event : SubmitEvent) => {
     event.preventDefault(); // prevent side reload when submitting the form
+    // select values
     const a : number = Number((document.querySelector("#a") as HTMLInputElement).value);
     const b : number = Number((document.querySelector("#b") as HTMLInputElement).value);
     const c : number = Number((document.querySelector("#c") as HTMLInputElement).value);
 
     const res : Response = await fetch("http://localhost:3000/add", {
         method: "POST",
-        mode: "cors", // This set CORS mode, not required herea
+        mode: "cors", // This set CORS mode, not required here but recommended
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({a, b, c}),
+        body: JSON.stringify({a, b, c}), // übertrage a, b, und c als json zum Server
     });
 
-    const data = await res.json();
+    const data = await res.json(); // parse das zurückgegeben JSON
 
-    console.log(res.status + " " + res.statusText);
+    console.log(res.status + " " + res.statusText); // Gebe den http statuscode in der Konsole aus
     if (res.ok) {
         const result : HTMLSpanElement = document.querySelector("#result") as HTMLSpanElement;
         if (result) {
@@ -25,6 +27,9 @@ addForm.addEventListener("submit", async (event : SubmitEvent) => {
     }
 })
 
+
+
+// Aufgabe 1:
 document.addEventListener("DOMContentLoaded", async () => { // Lade GET Route /cookies , wenn die Seite vollständig geladen wurde
     const res : Response = await fetch("http://localhost:3000/cookies", { // Rufe Route /cookies auf dem lokalen Server auf Port 3000 auf
         method: "GET", // GET request
@@ -33,3 +38,39 @@ document.addEventListener("DOMContentLoaded", async () => { // Lade GET Route /c
     });
     console.log(res.status + " " + res.statusText); // Gebe den HTTP status code und den status text des fetch()-Aufrufs in der Konsole aus
 });
+
+const getCookiesBtn : HTMLButtonElement = document.querySelector("#displayCookiesBtn") as HTMLButtonElement;
+const displayCookies : HTMLElement = document.querySelector("#displayCookies ") as HTMLInputElement;
+getCookiesBtn.addEventListener("click", async () => {
+    displayCookies.textContent = document.cookie; // schreibe cookies in das feld
+});
+
+const hideCookiesBtn : HTMLButtonElement = document.querySelector("#hideCookiesBtn") as HTMLButtonElement;
+hideCookiesBtn.addEventListener("click",  () => {
+    displayCookies.textContent = ""; // setze das Feld zurück
+})
+
+
+
+// Aufgabe 2:
+// Selektiere login und logout btn
+const loginBtn : HTMLButtonElement = document.querySelector("#login") as HTMLButtonElement;
+const logoutBtn : HTMLButtonElement = document.querySelector("#logout") as HTMLButtonElement;
+loginBtn.addEventListener("click",  async (evt: MouseEvent) => { // wenn der login btn geklickt wird
+    evt.preventDefault(); // prevents side reloading
+    const username : string = (document.querySelector("#userName") as HTMLInputElement).value; // lese wert des Inputs Username
+    // console.log(username);
+    if (username != null) { // rufe die Fnktn. nur auf, wenn ein Wert in username steht
+        const res : Response = await fetch("http://localhost:3000/login", {
+            method: "POST",
+            mode: "cors", // This set CORS mode, not required here but recommended
+            credentials : "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({username}), // übertrage a, b, und c als json zum Server
+        });
+        console.log(res.status + " " + res.statusText);
+    }
+})
+
